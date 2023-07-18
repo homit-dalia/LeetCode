ca25c045e43249187f1bed4dd796dd957fdfa7fb
Homit Dalia <homitdalia59@gmail.com>
2023-07-18 12:48:52 +0530

    Create 14.longest-common-prefix.py

---
+++ 14.longest-common-prefix.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=14 lang=python
+#
+# [14] Longest Common Prefix
+#
+
+# @lc code=start
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        res = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
+
+# @lc code=end
+
