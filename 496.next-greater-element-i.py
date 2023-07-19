069eae23423d48abfba7f271c49296176dc97ab2
Homit Dalia <homitdalia59@gmail.com>
2023-07-19 11:39:14 +0530

    Create 496.next-greater-element-i.py

---
+++ 496.next-greater-element-i.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=496 lang=python
+#
+# [496] Next Greater Element I
+#
+
+# @lc code=start
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        
+# @lc code=end
+
